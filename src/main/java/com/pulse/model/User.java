e382eead38483dbd2f02398d30e75891ccf228ec
PrinzeZ <princezundol@gmail.com>
2026-09-13 23:24:53 +0530

    Fix JPA config, add repositories , ready for integration

--- src/main/java/com/pulse/model/User.java
+++ src/main/java/com/pulse/model/User.java
@@ -40,5 +40,5 @@ public abstract class User {
     public String getUsername () { return username;}
     public void setUsername(String username) {this.username = username ;}
     public String getPassword() {return password;}
-    public void ssetPassword(String password) { this.password = password; }
+    public void setPassword(String password) { this.password = password; }
 }
--- src/test/java/com/pulse/PulseApplicationTests.java
+++ src/test/java/com/pulse/PulseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pulse.pulse;
+package com.pulse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
